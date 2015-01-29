8db3bfe12998b1b218c5eb5dca6d0d9b47fc9bcb
kellyka <kellyka@live.unc.edu>
2015-01-29 08:35:15 -0500

    adding files

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var userName = "Kaitlyn";
+var todayDate = new Date();
+
+function myFunction(){
+    document.write("Hello ");
+    document.write(userName);
+    console.log(todayDate);
+}
+
+
+document.getElementById("demo").innerHTML="This is the new info";
+document.getElementById("example").innerHTML="Today is January 27, 2015";
+myFunction();
+
+var myNumberVariable = 5;
+var myTextVariable = "My variable";
+    
+console.log(myNumberVariable);
+console.log(myTextVariable); 
